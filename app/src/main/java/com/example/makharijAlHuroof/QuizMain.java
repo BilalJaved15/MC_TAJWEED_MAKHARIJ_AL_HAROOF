1c6729db977e52e856e40149b2749e266bdffd3c
BilalJaved15 <bcsf18m029@pucit.edu.pk>
2021-11-09 22:02:34 +0500

    Added landscape mode for Tutorial activity

---
+++ app/src/main/java/com/example/makharijAlHuroof/QuizMain.java
@@ -0,0 +1,14 @@
+package com.example.makharijAlHuroof;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_main);
+    }
+}
